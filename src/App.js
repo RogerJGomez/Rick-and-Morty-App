8eebe4291d9412f728bfa9d365cb873b2976e78a
Roger Gómez <gomez.roger779@gmail.com>
2020-08-01 19:08:00 -0400

    Defined graphql api path and added provider

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'https://rickandmortyapi.com/graphql',
+  cache: new InMemoryCache()
+})
+
+const CHARACTERS = gql`
+  query GetCharacters {
+    characters {
+      results {
+        id
+        name
+      }
+    }
+  }
+`
+
+function ExchangeRates() {
+  const { loading, error, data } = useQuery(CHARACTERS)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
+
+  return data.characters.results.map(character => (
+    <div key={character.id}>
+      <p>
+        {character.id}: {character.name}
+      </p>
+    </div>
+  ))
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ApolloProvider client={client}>
+      <div>
+        <ExchangeRates />
+      </div>
+    </ApolloProvider>
+  )
 }
 
-export default App;
+export default App
